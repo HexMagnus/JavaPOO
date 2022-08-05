381c419ca84a8061d6a4676693ab826b75e68368
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-04 21:41:24 -0300

    Update

---
+++ Aula04/src/aula04/Caneta.java
@@ -0,0 +1,19 @@
+package aula04;
+
+public class Caneta {
+	
+	public String modelo;
+	public float ponta;
+	
+	public String getModelo() {
+		return this.modelo;
+	}
+	
+	public void setModelo(String m) {
+		this.modelo = m;
+		
+		
+	
+	}
+
+}
