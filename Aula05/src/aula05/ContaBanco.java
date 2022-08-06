5768f1181c979d369353ea5f1a6f4eaba3a7a6e4
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-05 22:48:56 -0300

    update

---
+++ Aula05/src/aula05/ContaBanco.java
@@ -0,0 +1,21 @@
+package aula05;
+
+public class ContaBanco {
+
+	public int numConta;
+	protected String tipo;
+	private String dono;
+	private float saldo;
+	private boolean status;
+	
+	
+	public void abrirConta() {
+		
+	}
+	
+	public void fecharConta() {
+		
+	}
+	
+	
+}
