be295df2d4f49a498b24a03397ce72e5f9a3a1d4
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-10 22:05:11 -0300

    Update

--- Aula06/src/aula06/ControleRemoto.java
+++ Aula06/src/aula06/ControleRemoto.java
@@ -1,6 +1,6 @@
 package aula06;
 
-public class ControleRemoto {
+public class ControleRemoto implements Controlador{
 
 
 		//atributos		
@@ -15,6 +15,102 @@ public class ControleRemoto {
 			this.ligado = false;
 			this.tocando= false;
 		}
+
+		
+		public int getVolume(){
+			return volume;
+		}
+		
+		public void setVolume(int volume) {
+			this.volume = volume;
+		}
+
+		public boolean getLigado() {
+			return ligado;
+		}
+
+		public void setLigado(boolean ligado) {
+			this.ligado = ligado;
+		}
+
+		public boolean getTocando() {
+			return tocando;
+		}
+
+		public void setTocando(boolean tocando) {
+			this.tocando = tocando;
+		}
+
+		//Métodos Abstratos
+		@Override
+		public void ligar() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void desligar() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void abrirMenu() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void fecharMenu() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void maisVolume() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void menosVolume() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void ligarMudo() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void desligarMudo() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void play() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+		@Override
+		public void pause() {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 
 
