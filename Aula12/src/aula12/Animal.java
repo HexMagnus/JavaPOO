3f45ea971d37805c10d4a0fe1bd9f677bfe9773e
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-20 23:07:19 -0300

    Update

--- Aula12/src/aula12/Animal.java
+++ Aula12/src/aula12/Animal.java
@@ -1,6 +1,6 @@
 package aula12;
 
-public class Animal {
+public abstract class Animal {
 	protected float peso;
 	protected int idade;
 	protected int membros;
@@ -8,6 +8,26 @@ public class Animal {
 	public abstract void locomover();
 	public abstract void alimentar();
 	public abstract void emitirSom();
+	
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public int getMembros() {
+		return membros;
+	}
+	public void setMembros(int membros) {
+		this.membros = membros;
+	}
 
+	
 }
 
---
+++ Aula12/src/aula12/Peixe.java
@@ -0,0 +1,25 @@
+package aula12;
+
+public class Peixe extends Animal {
+	private String corEscama;
+	
+	@Override
+	public void locomover() {
+		System.out.println("Nadando");
+	}
+	
+	@Override
+	public void alimentar() {
+		System.out.println("Comendo substancias");
+	}
+	
+	@Override
+	public void emitirSom() {
+		System.out.println("Peixe não faz som");
+	}
+	
+	public void soltarBolha() {
+		System.out.println("Soltando Bolha");
+	}
+
+}
