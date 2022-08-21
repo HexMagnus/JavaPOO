2694a5a67fe9715fa2f6dd1827d97741970f6e56
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-20 22:54:24 -0300

    Update

---
+++ Aula12/src/aula12/Animal.java
@@ -0,0 +1,13 @@
+package aula12;
+
+public class Animal {
+	protected float peso;
+	protected int idade;
+	protected int membros;
+	
+	public abstract void locomover();
+	public abstract void alimentar();
+	public abstract void emitirSom();
+
+}
+
---
+++ Aula12/src/aula12/Aula_12.java
@@ -0,0 +1,5 @@
+package aula12;
+
+public class Aula_12 {
+
+}
