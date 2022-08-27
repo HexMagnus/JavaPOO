2c2a19c39c6101174371f7a824acad428bd7ea1a
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-27 09:33:46 -0300

    Update

--- Aula_13/src/aula13/Animal.java
+++ Aula_13/src/aula13/Animal.java
@@ -1,5 +1,11 @@
 package aula13;
 
-public class Animal {
-
-}
+public abstract class Animal {
+	protected float peso;
+	protected int idade;
+	protected int membros;
+	
+	
+	public abstract  void emitirSom();
+	
+	}
