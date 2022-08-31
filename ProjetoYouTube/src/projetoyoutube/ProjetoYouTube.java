133ece1cef5af027cc97cb6e758faa3bdd7deb77
Lucas Martins <93499789+HexMagnus@users.noreply.github.com>
2022-08-31 00:05:56 -0300

    Update

--- ProjetoYouTube/src/projetoyoutube/ProjetoYouTube.java
+++ ProjetoYouTube/src/projetoyoutube/ProjetoYouTube.java
@@ -15,9 +15,12 @@ public class ProjetoYouTube {
 		
 		Visualizacao vis[] = new Visualizacao [5];
 		vis[0] = new Visualizacao(g[0], v[2]);
+		vis[0].avaliar();
 		System.out.println(vis[0].toString());
+		
 		vis[1] = new Visualizacao(g[0], v[1]);
-		System.out.println(vis[0].toString());
+		vis[0].avaliar(87.0f);
+		System.out.println(vis[1].toString());
 	}
 
 }
